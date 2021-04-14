1bde72a8c3520f5214bf3dca26fe3b62d330c4eb
Anna Vargová <64069511+AnickaVargova@users.noreply.github.com>
2021-04-14 14:39:09 +0200

    utils added

---
+++ client/utils.js
@@ -0,0 +1,32 @@
+export const normalizeDate = (date) => {
+  if (date) {
+    return new Intl.DateTimeFormat("en-UK").format(new Date(date));
+  }
+};
+
+export const dateParser = (date, direction) => {
+  if (direction === "fromServer") {
+    return `${date.slice(-2)}/${date.slice(-5, -3)}/${date.slice(0, 4)}`;
+  } else if (direction === "toServer") {
+    return `${date.slice(6)}-${date.slice(3, 5)}-${date.slice(0, 2)}`;
+  }
+};
+
+export const transformData = (data) => {
+  return data
+    .filter((item) => !item.completed)
+    .map((item) => ({
+      name: item.title,
+      date: dateParser(item.date, "fromServer"),
+    }));
+};
+
+export const validateName = (input, todos) => {
+  for (let todo of todos) {
+    if (todo.name === input) {
+      alert("This name is already in use. Choose another name.");
+      return false;
+    }
+  }
+  return true;
+};
